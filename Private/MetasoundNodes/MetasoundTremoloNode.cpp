d903e895f9f49692248830dca04091fa28d1f01c
MNguyen <nali.minh.ng@gmail.com>
2022-11-15 11:30:19 -0800

    Add tremolo metasound node

---
+++ Private/MetasoundNodes/MetasoundTremoloNode.cpp
@@ -0,0 +1,146 @@
+#pragma once
+
+#include "MetasoundNodes/MetasoundTremoloNode.h"
+
+#define LOCTEXT_NAMESPACE "MetasoundStandardNodes_TremoloNode"
+
+namespace Metasound
+{
+	
+	//------------------------------------------------------------------------------------
+	// FTremoloOperator
+	//------------------------------------------------------------------------------------
+	FTremoloOperator::FTremoloOperator(const FOperatorSettings& InSettings,
+		const FAudioBufferReadRef& InAudioInput,
+		const FFloatReadRef& InLFORate,
+		const FFloatReadRef& InLFODepth)
+		//const FEnumTremoloReadRef& InTremoloType)
+		: AudioInput(InAudioInput)
+		, AudioOutput(FAudioBufferWriteRef::CreateNew(InSettings))
+		, LFORate(InLFORate)
+		, LFODepth(InLFODepth)
+		//, TremoloType(InTremoloType)
+	{
+		TremoloDSPProcessor.Init();
+		TremoloDSPProcessor.InverseSampleRate = 1 / InSettings.GetSampleRate();
+		TremoloDSPProcessor.LfoPhase = 0.0f;
+	}
+
+	//------------------------------------------------------------------------------------
+	// GET INPUTS, OUTPUTS and EXECUTE
+	//------------------------------------------------------------------------------------
+	FDataReferenceCollection FTremoloOperator::GetInputs() const
+	{
+		using namespace TremoloNode;
+
+		FDataReferenceCollection InputDataReferences;
+		
+		InputDataReferences.AddDataReadReference(METASOUND_GET_PARAM_NAME(InParamNameAudioInput), FAudioBufferReadRef(AudioInput));
+		InputDataReferences.AddDataReadReference(METASOUND_GET_PARAM_NAME(InParamNameLFORate), FFloatReadRef(LFORate));
+		InputDataReferences.AddDataReadReference(METASOUND_GET_PARAM_NAME(InParamNameLFODepth), FFloatReadRef(LFODepth));
+		//InputDataReferences.AddDataReadReference(METASOUND_GET_PARAM_NAME(InParamNameTremoloType), FEnumTremoloReadRef(TremoloType));
+
+		return InputDataReferences;
+	}
+
+	FDataReferenceCollection FTremoloOperator::GetOutputs() const
+	{
+		using namespace TremoloNode;
+
+		FDataReferenceCollection OutputDataReferences;
+		OutputDataReferences.AddDataReadReference(METASOUND_GET_PARAM_NAME(OutParamNameAudio), FAudioBufferReadRef(AudioOutput));
+		return OutputDataReferences;
+	}
+
+	
+	void FTremoloOperator::Execute()
+	{
+		const float* InputAudio = AudioInput->GetData();
+		const int32 NumSamples = AudioInput->Num();
+		float* OutputAudio = AudioOutput->GetData();
+
+		TremoloDSPProcessor.SetLFODepth(*LFODepth);
+		TremoloDSPProcessor.SetLFORate(*LFORate);
+		//TremoloDSPProcessor.SetTremoloType(*TremoloType);
+
+		TremoloDSPProcessor.ProcessAudioBuffer(InputAudio, OutputAudio, NumSamples);
+	}
+
+
+	//------------------------------------------------------------------------------------
+	// NODE VERTEX and NODE INFO
+	//------------------------------------------------------------------------------------
+	const FNodeClassMetadata& FTremoloOperator::GetNodeInfo()
+	{
+		auto InitNodeInfo = []() -> FNodeClassMetadata
+		{
+			FNodeClassMetadata Info;
+
+			Info.ClassName = { TEXT("UE"), TEXT("Tremolo"), TEXT("Audio") };
+			Info.MajorVersion = 1;
+			Info.MinorVersion = 0;
+			Info.DisplayName = LOCTEXT("Metasound_TremoloDisplayName", "Tremolo");
+			Info.Description = LOCTEXT("Metasound_TremoloNodeDescription", "Applies tremolo effect to the audio input.");
+			Info.Author = PluginAuthor;
+			Info.PromptIfMissing = PluginNodeMissingPrompt;
+			Info.DefaultInterface = GetVertexInterface();
+			Info.CategoryHierarchy = { LOCTEXT("Metasound_TremoloNodeCategory", "Effects") };
+
+			return Info;
+		};
+
+		static const FNodeClassMetadata Info = InitNodeInfo();
+
+		return Info;
+	}
+
+	const FVertexInterface& FTremoloOperator::GetVertexInterface()
+	{
+		using namespace TremoloNode;
+		static const FVertexInterface Interface(
+			FInputVertexInterface(
+				TInputDataVertexModel<FAudioBuffer>(METASOUND_GET_PARAM_NAME_AND_METADATA(InParamNameAudioInput)),
+				TInputDataVertexModel<float>(METASOUND_GET_PARAM_NAME_AND_METADATA(InParamNameLFORate), 2.0f),
+				TInputDataVertexModel<float>(METASOUND_GET_PARAM_NAME_AND_METADATA(InParamNameLFODepth), 0.5f)
+				//TInputDataVertexModel<FEnumTremoloType>(METASOUND_GET_PARAM_NAME_AND_METADATA(InParamNameTremoloType), static_cast<int32>(SimpleDSP::ETremoloType::wSine))
+			),
+
+			FOutputVertexInterface(
+				TOutputDataVertexModel<FAudioBuffer>(METASOUND_GET_PARAM_NAME_AND_METADATA(OutParamNameAudio))
+			)
+		);
+
+		return Interface;
+	}
+
+	TUniquePtr<IOperator> FTremoloOperator::CreateOperator(const FCreateOperatorParams& InParams, FBuildErrorArray& OutErrors)
+	{
+		using namespace TremoloNode;
+
+		const FDataReferenceCollection& InputCollection = InParams.InputDataReferences;
+		const FInputVertexInterface& InputInterface = GetVertexInterface().GetInputInterface();
+
+		FAudioBufferReadRef AudioIn = InputCollection.GetDataReadReferenceOrConstruct<FAudioBuffer>(METASOUND_GET_PARAM_NAME(InParamNameAudioInput), InParams.OperatorSettings);
+		FFloatReadRef InLFORate = InputCollection.GetDataReadReferenceOrConstructWithVertexDefault<float>(InputInterface, METASOUND_GET_PARAM_NAME(InParamNameLFORate), InParams.OperatorSettings);
+		FFloatReadRef InLFODepth = InputCollection.GetDataReadReferenceOrConstructWithVertexDefault<float>(InputInterface, METASOUND_GET_PARAM_NAME(InParamNameLFODepth), InParams.OperatorSettings);
+		//FEnumTremoloReadRef InTremoloType = InputCollection.GetDataReadReferenceOrConstruct<FEnumTremoloType>(METASOUND_GET_PARAM_NAME(InParamNameTremoloType));
+
+		//return MakeUnique<FTremoloOperator>(InParams.OperatorSettings, AudioIn, InLFORate, InLFODepth, InTremoloType);
+		return MakeUnique<FTremoloOperator>(InParams.OperatorSettings, AudioIn, InLFORate, InLFODepth);
+	}
+
+
+	//------------------------------------------------------------------------------------
+	// FTremoloNode
+	//------------------------------------------------------------------------------------
+	FTremoloNode::FTremoloNode(const FNodeInitData& InitData)
+		: FNodeFacade(InitData.InstanceName, InitData.InstanceID, TFacadeOperatorClass<FTremoloOperator>())
+	{
+
+	}
+
+
+	METASOUND_REGISTER_NODE(FTremoloNode)
+}
+
+#undef LOCTEXT_NAMESPACE
---
+++ Public/MetasoundNodes/MetasoundTremoloNode.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include "MetasoundParamHelper.h"
+#include "MetasoundEnumRegistrationMacro.h"
+#include "SimpleDSP/Tremolo.h"
+
+namespace Metasound
+{
+
+#define LOCTEXT_NAMESPACE "MetasoundStandardNodes_TremoloNode"
+
+
+	namespace TremoloNode
+	{
+		METASOUND_PARAM(InParamNameAudioInput, "In", "Audio input.")
+		METASOUND_PARAM(InParamNameLFORate, "Rate", "Frequency of the LFO [0.1Hz - 10Hz]. Default is 2Hz.")
+		METASOUND_PARAM(InParamNameLFODepth, "Depth", "Intensity of the LFO [0.0 - 1.0]. Default is 0.5")
+		//METASOUND_PARAM(InParamNameTremoloType, "Type", "LFO type: Sine, Triangle, Saw, Square")
+
+		METASOUND_PARAM(OutParamNameAudio, "Out", "Audio output.")
+	}
+
+	/*DECLARE_METASOUND_ENUM(SimpleDSP::ETremoloType, SimpleDSP::ETremoloType::wSine, GUITAREFFECTPLUGIN_API, FEnumTremoloType, FEnumTremoloTypeInfo, FEnumTremoloReadRef, FEnumTremoloWriteRef);
+
+	DEFINE_METASOUND_ENUM_BEGIN(SimpleDSP::ETremoloType, FEnumTremoloType, "TremoloType")
+		DEFINE_METASOUND_ENUM_ENTRY(SimpleDSP::ETremoloType::wSine,		"SineDescription",		"Sine",		"SineLFO",		"Sine LFO Waveform"),
+		DEFINE_METASOUND_ENUM_ENTRY(SimpleDSP::ETremoloType::wTriangle,	"TriangleDescription",	"Triangle",	"TriangleLFO",	"Triangle LFO Waveform"),
+		DEFINE_METASOUND_ENUM_ENTRY(SimpleDSP::ETremoloType::wSaw,		"SawDescription",		"Saw",		"SawLFO",		"Saw LFO Waveform"),
+		DEFINE_METASOUND_ENUM_ENTRY(SimpleDSP::ETremoloType::wSquare,	"SquareDescription",	"Square",	"SquareLFO",	"Square LFO Waveform")
+	DEFINE_METASOUND_ENUM_END()*/
+
+#undef LOCTEXT_NAMESPACE
+
+	
+	//------------------------------------------------------------------------------------
+	// FTremoloOperator
+	//------------------------------------------------------------------------------------
+	class FTremoloOperator : public TExecutableOperator<FTremoloOperator>
+	{
+	public:
+		static const FNodeClassMetadata& GetNodeInfo();
+		static const FVertexInterface& GetVertexInterface();
+		static TUniquePtr<IOperator> CreateOperator(const FCreateOperatorParams& InParams, FBuildErrorArray& OutErrors);
+
+		FTremoloOperator(const FOperatorSettings& InSettings,
+			const FAudioBufferReadRef& InAudioInput,
+			const FFloatReadRef& InLFODepth,
+			const FFloatReadRef& InLFORate
+			//const FEnumTremoloReadRef& InTremoloType
+		);
+
+		virtual FDataReferenceCollection GetInputs() const override;
+		virtual FDataReferenceCollection GetOutputs() const override;
+
+		void Execute();
+
+
+	private:
+		// The input audio buffer and audio output
+		FAudioBufferReadRef AudioInput;
+		FAudioBufferWriteRef AudioOutput;
+
+		// The user-defined settings: rate, depth, type
+		FFloatReadRef LFORate;
+		FFloatReadRef LFODepth;
+		//FEnumTremoloReadRef TremoloType;
+
+		//DSP
+		SimpleDSP::FTremolo TremoloDSPProcessor;
+	};
+
+
+	//------------------------------------------------------------------------------------
+	// FTremoloNode
+	//------------------------------------------------------------------------------------
+	class FTremoloNode : public FNodeFacade
+	{
+	public:
+		FTremoloNode(const FNodeInitData& InitData);
+	};
+}
